7084be28f61be7c1bc6de04f3a80fe0308ccba36
Jason <el.jsteer@gmail.com>
2022-09-17 19:15:40 +1000

    updated anc completed add and delete Friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ module.exports = {
       .then(async (Users) => {
         const userObj = {
           Users,
-          friendCount: await friendCount(),
+          // friendCount: await friendCount(),
         };
         return res.json(userObj);
       })
@@ -99,27 +99,29 @@ module.exports = {
 
   // Add an Friend to a user
   addFriend(req, res) {
-    console.log('You are adding an Friend');
-    console.log(req.body);
+    console.log('You are adding a Friend');
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $addToSet: { friends: req.body } },
+      { $addToSet: { friends: req.params.friendId } },
       { runValidators: true, new: true }
     )
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: 'No user found with that ID :(' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+      .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+},
+
   // Remove Friend from a user
   deleteFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friend: { friendId: req.params.friendId } } },
+      { $pull: { friends: req.params.friendId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
